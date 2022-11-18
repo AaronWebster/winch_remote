9785ed93948b195671476e35adb1ff27341c692f
Aaron Webster <webstera@google.com>
2022-11-17 20:07:57 -0800

    Revert "Formatting."
    This reverts commit 8564ba7b3d0ca61519d846b5727ea5cc2ab216f8.

--- remote/remote.ino
+++ remote/remote.ino
@@ -1,7 +1,6 @@
-#include <WiFi.h>
 #include <esp_now.h>
-#include <esp_wifi.h>
-
+#include <WiFi.h>
+#include <esp_wifi.h> // only for esp_wifi_set_channel()
 #include "packets.emb.h"
 
 // Global copy of slave
@@ -17,7 +16,8 @@ void InitESPNow() {
   WiFi.disconnect();
   if (esp_now_init() == ESP_OK) {
     Serial.println("ESPNow Init Success");
-  } else {
+  }
+  else {
     Serial.println("ESPNow Init Failed");
     // Retry InitESPNow, add a counte and then restart?
     // InitESPNow();
@@ -26,10 +26,11 @@ void InitESPNow() {
   }
 }
 
+
+
 // Scan for slaves in AP mode
 void ScanForSlave() {
-  int16_t scanResults = WiFi.scanNetworks(false, false, false, 300,
-                                          CHANNEL);  // Scan only on one channel
+  int16_t scanResults = WiFi.scanNetworks(false, false, false, 300, CHANNEL); // Scan only on one channel
   // reset on each scan
   bool slaveFound = 0;
   memset(&slave, 0, sizeof(slave));
@@ -38,9 +39,7 @@ void ScanForSlave() {
   if (scanResults == 0) {
     Serial.println("No WiFi devices in AP Mode found");
   } else {
-    Serial.print("Found ");
-    Serial.print(scanResults);
-    Serial.println(" devices ");
+    Serial.print("Found "); Serial.print(scanResults); Serial.println(" devices ");
     for (int i = 0; i < scanResults; ++i) {
       // Print SSID and RSSI for each device found
       String SSID = WiFi.SSID(i);
@@ -61,27 +60,17 @@ void ScanForSlave() {
       if (SSID.indexOf("Slave") == 0) {
         // SSID of interest
         Serial.println("Found a Slave.");
-        Serial.print(i + 1);
-        Serial.print(": ");
-        Serial.print(SSID);
-        Serial.print(" [");
-        Serial.print(BSSIDstr);
-        Serial.print("]");
-        Serial.print(" (");
-        Serial.print(RSSI);
-        Serial.print(")");
-        Serial.println("");
+        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
         // Get BSSID => Mac Address of the Slave
         int mac[6];
-        if (6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x", &mac[0], &mac[1],
-                        &mac[2], &mac[3], &mac[4], &mac[5])) {
-          for (int ii = 0; ii < 6; ++ii) {
-            slave.peer_addr[ii] = (uint8_t)mac[ii];
+        if ( 6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x",  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5] ) ) {
+          for (int ii = 0; ii < 6; ++ii ) {
+            slave.peer_addr[ii] = (uint8_t) mac[ii];
           }
         }
 
-        slave.channel = CHANNEL;  // pick a channel
-        slave.encrypt = 0;        // no encryption
+        slave.channel = CHANNEL; // pick a channel
+        slave.encrypt = 0; // no encryption
 
         slaveFound = 1;
         // we are planning to have only one slave in this example;
@@ -112,7 +101,7 @@ bool manageSlave() {
     Serial.print("Slave Status: ");
     // check if the peer exists
     bool exists = esp_now_is_peer_exist(slave.peer_addr);
-    if (exists) {
+    if ( exists) {
       // Slave already paired.
       Serial.println("Already Paired");
       return true;
@@ -172,6 +161,7 @@ void deletePeer() {
 uint64_t sequence_id = 0;
 uint8_t packet_buf[Packet::IntrinsicSizeInBytes()];
 
+
 void sendData() {
   memset(packet_buf, 0, sizeof(packet_buf));
   auto packet = MakePacketView(packet_buf, Packet::IntrinsicSizeInBytes());
@@ -179,8 +169,7 @@ void sendData() {
   packet.direction().Write(Direction::FORWARD);
   const uint8_t *peer_addr = slave.peer_addr;
   Serial.println("Sending: ");
-  esp_err_t result = esp_now_send(peer_addr, packet.BackingStorage().data(),
-                                  packet.SizeInBytes());
+  esp_err_t result = esp_now_send(peer_addr, packet.BackingStorage().data(), packet.SizeInBytes());
   Serial.print("Send Status: ");
   if (result == ESP_OK) {
     Serial.println("Success");
@@ -203,28 +192,22 @@ void sendData() {
 // callback when data is sent from Master to Slave
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x", mac_addr[0],
-           mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.print("Last Packet Sent to: ");
-  Serial.println(macStr);
-  Serial.print("Last Packet Send Status: ");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success"
-                                                : "Delivery Fail");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Sent to: "); Serial.println(macStr);
+  Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 void setup() {
-  // Heltec.begin(true /*DisplayEnable Enable*/, true /*LoRa Disable*/, true
-  // /*Serial Enable*/, false /*PABOOST Enable*/, 470E6 /**/);
+   // Heltec.begin(true /*DisplayEnable Enable*/, true /*LoRa Disable*/, true /*Serial Enable*/, false /*PABOOST Enable*/, 470E6 /**/);
   Serial.begin(115200);
-  // Set device in STA mode to begin with
+  //Set device in STA mode to begin with
   WiFi.mode(WIFI_STA);
   esp_wifi_set_channel(CHANNEL, WIFI_SECOND_CHAN_NONE);
   Serial.println("ESPNow/Basic/Master Example");
   // This is the mac address of the Master in Station Mode
-  Serial.print("STA MAC: ");
-  Serial.println(WiFi.macAddress());
-  Serial.print("STA CHANNEL ");
-  Serial.println(WiFi.channel());
+  Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
+  Serial.print("STA CHANNEL "); Serial.println(WiFi.channel());
   // Init ESPNow with a fallback logic
   InitESPNow();
   // Once ESPNow is successfully Init, we will register for Send CB to
@@ -236,11 +219,11 @@ bool isPaired = false;
 
 void loop() {
   // In the loop we scan for slave
-  if (!isPaired) ScanForSlave();
-
+  if(!isPaired) ScanForSlave();
+  
   // If Slave is found, it would be populate in `slave` variable
   // We will check if `slave` is defined and then we proceed further
-  if (slave.channel == CHANNEL) {  // check if slave channel is defined
+  if (slave.channel == CHANNEL) { // check if slave channel is defined
     // `slave` is defined
     // Add slave as peer if it has not been added already
     isPaired = manageSlave();
@@ -252,7 +235,8 @@ void loop() {
       // slave pair failed
       Serial.println("Slave pair failed!");
     }
-  } else {
+  }
+  else {
     // No slave found to process
   }
 
